eba328e711e51494ed0be323940252d8664f589c
Mohit Singh <mohitkrsingh2603@gmail.com>
2026-07-16 10:35:24 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const attendanceRoutes = require("./routes/attendanceRoutes");
 const leaveRoutes = require("./routes/leaveRoutes");
 const holidayRoutes = require("./routes/holidayRoutes");
 const settingsRoutes = require("./routes/settingsRoutes");
+const payrollRoutes = require("./routes/payrollRoutes");
 
 // Middleware
 app.use(cors());
@@ -60,6 +61,7 @@ app.use("/api/attendance", attendanceRoutes);
 app.use("/api/leave", leaveRoutes);
 app.use("/api/holidays", holidayRoutes);
 app.use("/api/settings", settingsRoutes);
+app.use("/api/payroll", payrollRoutes);
 
 connectDB();
 
